e1faa0af3925b7736236948a86b47efc0bf3e1a0
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 09:16:02 -0600

    Test Attendee add_interest method

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -27,4 +27,16 @@ describe Attendee do
       expect(attendee.interests).to eq []
     end
   end
+
+  describe '#add_interest' do
+    it 'adds interests to the interests array' do
+      attendee = Attendee.new('Bob', 20)
+
+      attendee.add_interest('Bumper Cars')
+      attendee.add_interest('Ferris Wheel')
+
+      expected = ["Bumper Cars", "Ferris Wheel"]
+      expect(attendee.interests).to eq expected
+    end
+  end
 end

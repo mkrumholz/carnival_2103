5c43819d0fa4a47955912b9dda8c0ede6751989a
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 09:25:38 -0600

    Create test for Carnival add_ride method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -23,4 +23,20 @@ describe Carnival do
       expect(jeffco_fair.rides).to eq []
     end
   end
+
+  describe '#add_ride' do
+    it 'adds rides to rides array' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      expected = [ferris_wheel, bumper_cars, scrambler]
+      expect(jeffco_fair.rides).to eq expected
+    end
+  end
 end

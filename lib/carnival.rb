604764645073e7a55081858b4394a2f8f2239a19
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 10:12:06 -0600

    Build ticket_lottery_contestants method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -25,9 +25,6 @@ class Carnival
 
   def attendees_by_ride_interest
     all_rides = @rides.reduce({}) do |all_rides, ride|
-      # interested = @attendees.find_all do |attendee|
-      #   attendee.interests.include?(ride.name)
-      # end
       interested = interested_riders(ride)
       all_rides.update(ride => interested)
     end
@@ -39,7 +36,10 @@ class Carnival
     end
   end
 
-  def ticket_lottery_contestants
-
+  def ticket_lottery_contestants(ride)
+    interested = interested_riders(ride)
+    interested.find_all do |attendee|
+      attendee.spending_money < ride.cost
+    end
   end
 end

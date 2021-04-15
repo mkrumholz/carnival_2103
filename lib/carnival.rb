2fbec66be26e358a50f2707367513080135a7154
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 09:43:22 -0600

    Build attendees variable and admit method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,10 +1,12 @@
 class Carnival
   attr_reader :name,
-              :rides
+              :rides,
+              :attendees
 
   def initialize(name)
     @name = name
     @rides = []
+    @attendees = []
   end
 
   def add_ride(ride)
@@ -15,5 +17,9 @@ class Carnival
     @rides.find_all do |ride|
       attendee.interests.include?(ride.name)
     end
+  end
+
+  def admit(attendee)
+    @attendees << attendee
   end
 end

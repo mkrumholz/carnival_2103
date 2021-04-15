eef83bdcf1b01dfd9cfafde64e28eeb50778c278
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 10:42:35 -0600

    Create test for case with no lottery winner

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -251,5 +251,29 @@ describe Carnival do
       expected = "📣 Johnny has won a ticket for the Bumper Cars! 🥳"
       expect(jeffco_fair.announce_lottery_winner(bumper_cars)).to eq expected
     end
+
+    it 'returns no winners annoucement if no contestants available' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      bob = Attendee.new('Bob', 0)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new('Johnny', 5)
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Bumper Cars')
+      johnny.add_interest('Bumper Cars')
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+
+      expected = "No winners for this lottery"
+      expect(jeffco_fair.announce_lottery_winner(ferris_wheel)).to eq expected
+    end
   end
 end

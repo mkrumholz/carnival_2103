bc382735e6ae71a05e53d74abf952dc4be709a31
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 10:25:37 -0600

    Create positive test for announce_lottery_winner method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -198,7 +198,7 @@ describe Carnival do
       jeffco_fair.admit(johnny)
 
       winner = jeffco_fair.draw_lottery_winner(bumper_cars)
-      expect(jeffco_fair.ticket_lottery_contestants.include?(winner)).to be true
+      expect(jeffco_fair.ticket_lottery_contestants(bumper_cars).include?(winner)).to be true
     end
 
     it 'returns a lottery winner from the contestants list' do
@@ -222,6 +222,33 @@ describe Carnival do
       jeffco_fair.admit(johnny)
 
       expect(jeffco_fair.draw_lottery_winner(ferris_wheel)).to be_nil
+    end
+  end
+
+  describe '#announce_lottery_winner' do
+    it 'returns an announcement of the winner for a specific ride' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      bob = Attendee.new('Bob', 0)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new('Johnny', 5)
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Bumper Cars')
+      johnny.add_interest('Bumper Cars')
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+      allow(jeffco_fair).to receive(:draw_lottery_winner) {[johnny]}
+
+      expected = "📣 Johnny has won a ticket for the Bumper Cars! 🥳"
+      expect(jeffco_fair.announce_lottery_winner(ride)).to eq expected
     end
   end
 end

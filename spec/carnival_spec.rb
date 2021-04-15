9cd4d426d9071e4dd48c9517b3b3b21112838339
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-15 09:42:13 -0600

    Create tests for attendees list and admit method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -22,6 +22,12 @@ describe Carnival do
 
       expect(jeffco_fair.rides).to eq []
     end
+
+    it 'starts with no attendees' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      expect(jeffco_fair.attendees).to eq []
+    end
   end
 
   describe '#add_ride' do
@@ -60,6 +66,32 @@ describe Carnival do
       sally_expected = [scrambler]
       expect(jeffco_fair.recommend_rides(bob)).to eq bob_expected
       expect(jeffco_fair.recommend_rides(sally)).to eq sally_expected
+    end
+  end
+
+  describe '#admit' do
+    it 'adds attendees to the attendees array' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      bob = Attendee.new('Bob', 20)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new('Johnny', 5)
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Bumper Cars')
+      johnny.add_interest('Bumper Cars')
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+
+      expected = [bob, sally, johnny]
+      expect(jeffco_fair.attendees).to eq expected
     end
   end
 end
